d50268dcaa5d9bf111339804a02ca7bf19462deb
kangsworkspace <141600830+kangsworkspace@users.noreply.github.com>
2023-12-01 16:01:36 +0900

    UI 다듬기 전

---
+++ unitChange/TemperatureModel.swift
@@ -0,0 +1,18 @@
+//
+//  OnDoModel.swift
+//  unitChange
+//
+//  Created by Kang on 12/1/23.
+//
+
+import Foundation
+
+class OnDoModel {
+    
+    var OnDo: Double = 0.0
+    
+    func On
+    
+    
+    
+}
---
+++ unitChange/TemperatureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TemperatureViewController.swift
+//  unitChange
+//
+//  Created by Kang on 12/1/23.
+//
+
+import UIKit
+
+class TemperatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
